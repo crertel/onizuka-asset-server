973cb51420d189769a0e1b254f4d2ec222d02dd6
Paul Kwiatkowski <pnkwiatkowski@gmail.com>
2013-10-22 00:06:42 -0500

    Initial rails project commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OnizukaAssetServer::Application.config.secret_key_base = '4eb4c31e1281c04711a0a79c86b346adcbf9d6da5b85a00bd9f240569c9ea108d840159eca30be26c6e6159f22d1471adc3416f8f56427677e596d7546a89673'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OnizukaAssetServer::Application.config.session_store :cookie_store, key: '_onizuka-asset-server_session'
