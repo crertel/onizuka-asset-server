52ba62063f406e37aa829f6927683ecdddcbd97d
Paul Kwiatkowski <pnkwiatkowski@gmail.com>
2014-01-26 06:44:21 -0600

    Added specific routes for asset subclasses.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 OnizukaAssetServer::Application.routes.draw do
 
   resources :assets
+  resources :image_assets,  controller: 'assets'
+  resources :mesh_assets,   controller: 'assets'
+  resources :sound_assets,  controller: 'assets'
+  resources :script_assets, controller: 'assets'
+  resources :markup_assets, controller: 'assets'
 
   get '/files/:id_or_name',
       to: 'files#get',
